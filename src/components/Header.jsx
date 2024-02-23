7404a42101a97a8c1ec8c6dbb1ea159cb357a3b8
myfirstgit <9313265870abcd@gmail.com>
2024-02-23 11:34:51 +0530

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
    
       <header className='w-screen bg-white flex justify-center items-center  border shadow-md py-4 fixed top-0 '>
-        <h1 className='text-3xl font-bold uppercase'>Vijay Rathod Blogs</h1>
+        <h1 className='text-3xl font-bold uppercase'>Vijay Blogs</h1>
       </header>
      
   )
